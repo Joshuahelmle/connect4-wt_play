185565441dd70ce6f1df9408caede7d7461c30d1
vanessa510 <vanessa.predovic@htwg-konstanz.de>
2020-11-24 16:02:41 +0100

    zoom columns of board in and out.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,3 +3,26 @@
 function setColor(color, cell) {
     document.getElementById(cell.id).style.backgroundColor = color;
 }
+
+function zoomIn(event, cell, sizeOfRows) {
+    var cells = document.getElementsByClassName("cell");
+    var colOfHoveredCell = cell.id.substr(cell.id.indexOf(",") + 1, 1);
+    var rowCounter = 0;
+
+        for(var i = 0; i < cells.length; i++) {
+        var col = cells[i].id.substr(cells[i].id.indexOf(",") + 1, 1);
+        var currentRow = cells[i].id.substr(cells[i].id.indexOf(",") - 1, 1);
+        if (rowCounter.toString() === currentRow && colOfHoveredCell === col && rowCounter <= sizeOfRows) {
+            cells[i].style.transform = 'scale(1.5)';
+            rowCounter += 1;
+        }
+
+    }
+}
+
+function zoomOut() {
+    var element = document.getElementsByClassName("cell");
+    for (var i = 0; i < element.length; i++) {
+        element[i].style.transform = "none";
+    }
+}
