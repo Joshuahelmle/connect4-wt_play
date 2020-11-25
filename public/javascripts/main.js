ddd2ffe18ce40387b734aa4b77ff6f704cd00d79
vanessa510 <vanessa.predovic@htwg-konstanz.de>
2020-11-25 12:43:25 +0100

    show preview of hovered column.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,10 @@
 
 
 function setColor(color, cell) {
-    document.getElementById(cell.id).style.backgroundColor = color;
+    document.getElementById(cell.id).classList.add(color === "red" ? "red" : "yellow")
 }
 
-function zoomIn(event, cell, sizeOfRows) {
+function zoomIn(event, cell, sizeOfRows, previewRowIdx) {
     var cells = document.getElementsByClassName("cell");
     var colOfHoveredCell = cell.id.substr(cell.id.indexOf(",") + 1, 1);
     var rowCounter = 0;
@@ -18,11 +18,22 @@ function zoomIn(event, cell, sizeOfRows) {
         }
 
     }
+
+    var previewColIdx = cell.id.substr(cell.id.indexOf(",") + 1, 1);
+    var cellToBeHighlighted = document.getElementById("cell-(" + previewRowIdx + ","+ previewColIdx + ")");
+    cellToBeHighlighted.classList.replace("unset", "preview");
+    cellToBeHighlighted.style.backgroundColor = 'darkred';
+
+
 }
 
 function zoomOut() {
     var element = document.getElementsByClassName("cell");
     for (var i = 0; i < element.length; i++) {
         element[i].style.transform = "none";
+
+        if(element[i].className === "cell preview") {
+            element[i].style.backgroundColor = '#f7eed5';
+        }
     }
 }
