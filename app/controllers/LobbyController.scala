f04fb97ee2726197314e9dbd30e5a1f019e5841b
Joshua Helmle <joshua.helmle@htwg-konstanz.de>
2020-12-18 17:06:15 +0100

    added websocket to Lobby and route for it

--- app/controllers/LobbyController.scala
+++ app/controllers/LobbyController.scala
@@ -1,16 +1,24 @@
 package controllers
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import akka.stream.Materializer
+import de.htwg.se.connect4.util.Observer
 import javax.inject.{Inject, Singleton}
-import play.api.mvc.{AnyContent, BaseController, ControllerComponents, Request}
+import play.api.libs.json
+import play.api.libs.json.{JsValue, Json}
+import play.api.libs.streams.ActorFlow
+import play.api.mvc.{AnyContent, BaseController, ControllerComponents, Request, WebSocket}
 
 @Singleton
 class LobbyController @Inject()(val controllerComponents: ControllerComponents) (implicit system : ActorSystem, mat: Materializer) extends BaseController {
 
   var games : Map[Int, GameController] = Map.empty[Int, GameController]
   var gameIdx = 0
-
+  object Connect4LobbyWebSocketActorFactory {
+    def create(out: ActorRef) = {
+      Props(new Connect4LobbyWebSocketActor(out))
+    }
+  }
 
 
   def newGame() = Action {implicit request : Request[AnyContent] =>
@@ -48,6 +56,51 @@ class LobbyController @Inject()(val controllerComponents: ControllerComponents) 
   def openSocket(idx: Int)   = {
     val game = games(idx)
     game.socket
+  }
+
+  def getGames() = {
+    Json.obj(
+      "games" -> Json.toJson(
+        for {
+          game <- games
+
+        } yield {
+          Json.obj(
+            "player1" -> game._2.controller.getPlayers(0).playerName,
+            "player2" -> game._2.controller.getPlayers(1).playerName,
+            "id" -> game._1
+          )
+        }
+
+      )
+    )
+  }
+
+  def createGame() = {
+
+  }
+
+  def socket = WebSocket.accept[JsValue,JsValue] { request =>
+    ActorFlow.actorRef {
+      out => Connect4LobbyWebSocketActorFactory.create(out)
+    }
+  }
+
+
+
+  class Connect4LobbyWebSocketActor(out : ActorRef) extends Actor {
+    override def receive: Receive = {
+      case msg: JsValue =>
+        println(msg)
+          val _type = (msg \ "_type").as[String]
+          _type match {
+            case "getGames" => out ! getGames
+            case "createGame" => out ! createGame
+      }
+
+    }
+
+
   }
 
 }
--- public/javascripts/connect4Vue.js
+++ public/javascripts/connect4Vue.js
@@ -35,12 +35,12 @@ $(document).ready(function () {
 Vue.component('buttongroup', {
     template: `
             <div class="list-group list-group-horizontal btn-group-above"> 
-            <button v-for="button in buttons" v-bind:id="button.id" class="btn btn-primary list-group-item list-group-item-action flex-fill"> {{button.text }} </button>
+            <button v-for="button in this.buttons" v-bind:id="button.id" class="btn btn-primary list-group-item list-group-item-action flex-fill"> {{button.text }} </button>
             </div>
     `,
     data: function () {
         return {
-            buttons: buttons
+            buttons:  [{text: "Restart Game",id: "btn_restart"}, {text: "Quit Game", id: "btn_quit"}, {text: "Save Game", id: "btn_save"}, {text: "Undo", id: "btn_undo"}, {text: "Redo", id: "btn_redo"}]
         }
     },
 
